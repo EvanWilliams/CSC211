678cdbdbf6eb59154ed3d9851a031cb501d201d1
Evan Williams <edw@evanwilliams.info>
2012-06-03 21:21:32 -0400

    turning in

--- ExceptionHandling/src/edw/emich/CSC211/ewilli46/ExceptionHandling/ExceptThis1.java
+++ ExceptionHandling/src/edw/emich/CSC211/ewilli46/ExceptionHandling/ExceptThis1.java
@@ -1,78 +1,78 @@
-package edw.emich.CSC211.ewilli46.ExceptionHandling;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-public class ExceptThis1 {
-
-	public static void returnRatio()throws InputMismatchException {
-
-		Scanner scan = new Scanner( System.in); 
-		int n1, n2; 
-		double r;{ 
-			n1 = scan.nextInt(); 
-			n2 = scan.nextInt(); 
-			r = ( double) n1 / n2; 
-
-		}
-
-		public static void main(String[] args)
-		{
-			
-			boolean hasNotEnteredTwoNumbers = true;
-			do {
-				System.out.println("Enter two integers separated by a new line. ");
-				try {
-					n1 = scan.nextInt();
-					System.out.println("The First integer is : " + n1 + "\n");
-					n2 = scan.nextInt();
-					System.out.println("The Second integer is : " + n2 + "\n");
-					r = (double) n1 / n2;
-					System.out.println("The Quotient of n1 and n2 is : " + r);
-					hasNotEnteredTwoNumbers = false;
-
-				} catch (InputMismatchException e) {
-					System.out.println("one of both of your integers is invalid \n");
-
-				} finally {
-					scan.nextLine();
-				}
-			} while (hasNotEnteredTwoNumbers);
-		}
-		//	Output using returnRatio:Enter two integers separated by a new line. 
-		//	f
-		//	one of both of your integers is invalid 
-		//
-		//	Enter two integers separated by a new line. 
-		//	g
-		//	one of both of your integers is invalid 
-		//
-		//	Enter two integers separated by a new line. 
-		//	z
-		//	one of both of your integers is invalid 
-		//
-		//	Enter two integers separated by a new line. 
-		//	111
-		//	The First integer is : 111
-		//
-		//	222
-		//	The Second integer is : 222
-		//
-		//	The Quotient of n1 and n2 is : 0.5
-
-		//	Output:
-		//Enter two integers separated by a new line. 
-		//	f
-		//	one of both of your integers is invalid 
-		//
-		//	Enter two integers separated by a new line. 
-		//	49
-		//	The First integer is : 49
-		//
-		//	126
-		//	The Second integer is : 126
-		//
-		//	The Quotient of n1 and n2 is : 0.3888888888888889
-
-	}
-}
+//package edw.emich.CSC211.ewilli46.ExceptionHandling;
+//
+//import java.util.InputMismatchException;
+//import java.util.Scanner;
+//
+//public class ExceptThis1 {
+//
+//	public static void returnRatio()throws InputMismatchException {
+//
+//		Scanner scan = new Scanner( System.in); 
+//		int n1, n2; 
+//		double r;{ 
+//			n1 = scan.nextInt(); 
+//			n2 = scan.nextInt(); 
+//			r = ( double) n1 / n2; 
+//
+//		}
+//
+//		public static void main(String[] args)
+//		{
+//			
+//			boolean hasNotEnteredTwoNumbers = true;
+//			do {
+//				System.out.println("Enter two integers separated by a new line. ");
+//				try {
+//					n1 = scan.nextInt();
+//					System.out.println("The First integer is : " + n1 + "\n");
+//					n2 = scan.nextInt();
+//					System.out.println("The Second integer is : " + n2 + "\n");
+//					r = (double) n1 / n2;
+//					System.out.println("The Quotient of n1 and n2 is : " + r);
+//					hasNotEnteredTwoNumbers = false;
+//
+//				} catch (InputMismatchException e) {
+//					System.out.println("one of both of your integers is invalid \n");
+//
+//				} finally {
+//					scan.nextLine();
+//				}
+//			} while (hasNotEnteredTwoNumbers);
+//		}
+//		//	Output using returnRatio:Enter two integers separated by a new line. 
+//		//	f
+//		//	one of both of your integers is invalid 
+//		//
+//		//	Enter two integers separated by a new line. 
+//		//	g
+//		//	one of both of your integers is invalid 
+//		//
+//		//	Enter two integers separated by a new line. 
+//		//	z
+//		//	one of both of your integers is invalid 
+//		//
+//		//	Enter two integers separated by a new line. 
+//		//	111
+//		//	The First integer is : 111
+//		//
+//		//	222
+//		//	The Second integer is : 222
+//		//
+//		//	The Quotient of n1 and n2 is : 0.5
+//
+//		//	Output:
+//		//Enter two integers separated by a new line. 
+//		//	f
+//		//	one of both of your integers is invalid 
+//		//
+//		//	Enter two integers separated by a new line. 
+//		//	49
+//		//	The First integer is : 49
+//		//
+//		//	126
+//		//	The Second integer is : 126
+//		//
+//		//	The Quotient of n1 and n2 is : 0.3888888888888889
+//
+//	}
+//}
--- IOlabPart1/src/edw/emich/CSC211/ewilli46/IOlabPart1/NoDuplicates.java
+++ IOlabPart1/src/edw/emich/CSC211/ewilli46/IOlabPart1/NoDuplicates.java
@@ -7,8 +7,10 @@ public class NoDuplicates {
 	/**
 	 * @param args
 	 */
+
 	public static void main(String[] args) {
 		String s;
+//		Try Statement to make sure that both files are accepted
 		try {
 			BufferedReader inputStream = new BufferedReader(new FileReader(
 					"./ExtraSpaces.txt"));
@@ -24,6 +26,7 @@ public class NoDuplicates {
 				System.out.println(count + ")" + s);
 				line = inputStream.readLine();
 			}
+//		Closing both streams at the end of the progr am..
 			inputStream.close();
 			outputStream.close();
 
@@ -34,17 +37,24 @@ public class NoDuplicates {
 			System.out.println("Error reading from original.txt");
 		}
 	}
-
+//		I thought to make a method that handled removing the extra "white space" from the given line of text, Saved as a
+//		String temporarily. THis works by finding two instances of ' '.
 	public static String RemoveWhitespace(String s) {
 		int First, len, Second;
 		len = s.length();
+//		THe second starts scanning from the index after the First instanceOf 
 		First = s.indexOf(' ');
 		Second = s.indexOf(' ', First + 1);
+//		NOw this is a bit screwy but i got it to work. I made the First index defined as the index of the Second-1
+//		, and the Second as the First-1. This made my program work so that after the extra white space was 
+//		effectively deleted I would not get stuck in a place where First=1 and SEcond =4 and the ' ' at index 4 
+//		will always remain this taking that instance first.
 		do {
 			First = s.indexOf(' ', Second - 1);
 			Second = s.indexOf(' ', First + 1);
 			for (int i = 0; i < len - 1; i++) {
-
+//		FOr this part of the program I checked if the two instances of ' ' were in adjacent index places and deleted 
+//		the one that was farthest to the right of the string.
 				while (Second == First + 1) {
 					s = s.substring(0, First + 1)
 							+ s.substring(Second + 1, len);
@@ -53,8 +63,10 @@ public class NoDuplicates {
 					Second = s.indexOf(' ', (First + 1));
 				}
 			}
+//		Although this program may run a bit more times than it may necessarily need to it will stop the loop if there is
+//		any index value returned as a -1
 		} while (Second != -1 && First != -1);
-		// String s2 = s;
+//		returns a new string also named "s" since strings r immutable!!!
 		return s;
 
 	}
